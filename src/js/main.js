aea7d3c7eab8452dc84275451cdea7cf11593adf
dev-baekgom <76412624+dev-baekgom@users.noreply.github.com>
2021-01-25 21:05:47 +0900

    test

--- src/js/main.js
+++ src/js/main.js
@@ -141,7 +141,7 @@ function firebase_download_code(){
             })
           }
           else {
-            firebase.database().ref('html/code/'+ result + '/text').once('value').then((snapshot) => {
+            firebase.database().ref('html/code/'+ result + '/text').on('value', (snapshot) => {
               var text = snapshot.val();
               if(text == null){
                 bootbox.alert({
